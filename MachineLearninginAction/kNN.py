32b4b696f044c3313edf05f86b2e51f64fe0b603
Bearchat <283938162@qq.com>
2018-04-25 18:12:32 +0800

    PyDBPool新增对sqlserver的支持

--- MachineLearninginAction/kNN.py
+++ MachineLearninginAction/kNN.py
@@ -15,26 +15,59 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
 
     print(group, labels)
-    return group,labels
+    return group, labels
 
 
-group,labels = createDataSet()
+group, labels = createDataSet()
+
+'''
+inX 用于分类的输入向量
+dataSet 输入的训练样本集
+label 标签向量
+k  用于选择最近邻居的数目
+
+'''
+
 
 
 def classify0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]  # 矩阵行数
-    print('矩阵行数 = ',dataSetSize)
+    print('矩阵行数 = ', dataSetSize)
 
     print(np.tile(inX, (dataSetSize, 1)))  # tile 复制数组
 
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
 
-    print('diffMat =',diffMat)
+    print('diffMat =', diffMat)
 
-    sqDiffMat = diffMat ** 2 # 矩阵平方
-    
+    sqDiffMat = diffMat ** 2  # 矩阵平方
+
+    print('sqDiffMat =', sqDiffMat)
+    sqDistances = sqDiffMat.sum(axis=1)
+
+    print('sqDistances = ', sqDistances)
+    distances = sqDistances ** 0.5 # 开根号
+    print('distances = ', sqDistances)
+
+    sortedDistIndicies = distances.argsort()
+
+
+    classCount =  {}
+
+    for i in range(k):
+        votelabel = labels[sortedDistIndicies[i]]
+        classCount[votelabel] = classCount.get(votelabel,0) + 1
+
+
+    # 计算完所有点之间的距离之后,对数据按照从小到大的次序排序  然后确定前k个距离最小的元素所在的主要分类
+    # key=operator.itemgetter(1) 按照元组的第二个元素进行排序
+    sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+
+    return sortedClassCount[0][0]
 
 
 
 
-classify0([0,0],group,labels,3)
+res = classify0([0, 0], group, labels, 3)
+
+print('res = ',res)
